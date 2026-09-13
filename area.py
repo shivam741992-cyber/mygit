62da72e42dbee5423c028c264a3c3fbc95ed0af9
Shivam <shivam741992@gmail.com>
2026-09-13 15:48:45 +0530

    created area pytjon fun

---
+++ area.py
@@ -0,0 +1,12 @@
+def area_of_circle(radius):
+    """
+    Calculate the area of a circle given its radius.
+
+    Parameters:
+    radius (float): The radius of the circle.
+
+    Returns:
+    float: The area of the circle.
+    """
+    import math
+    return math.pi * (radius ** 2)
